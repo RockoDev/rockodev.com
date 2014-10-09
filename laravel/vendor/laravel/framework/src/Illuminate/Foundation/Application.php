5a6fe865c9c610a53905a0a9cfb40fc0b5080e66
RockoDev <contacto@rockodev.com>
2014-10-08 22:13:00 -0500

    more larvel changes

--- laravel/vendor/laravel/framework/src/Illuminate/Foundation/Application.php
+++ laravel/vendor/laravel/framework/src/Illuminate/Foundation/Application.php
@@ -189,14 +189,27 @@ class Application extends Container implements HttpKernelInterface, TerminableIn
 	 */
 	public function bindInstallPaths(array $paths)
 	{
-		$this->instance('path', realpath($paths['app']));
+		if (realpath($paths['app'])) {
+			$this->instance('path', realpath($paths['app']));
+		}
+		elseif (file_exists($paths['app'])) {
+			$this->instance('path', $paths['app']);
+		}
+		else {
+			$this->instance('path', FALSE);
+		}
 
-		// Here we will bind the install paths into the container as strings that can be
-		// accessed from any point in the system. Each path key is prefixed with path
-		// so that they have the consistent naming convention inside the container.
 		foreach (array_except($paths, array('app')) as $key => $value)
 		{
-			$this->instance("path.{$key}", realpath($value));
+			if (realpath($value)) {
+				$this->instance("path.{$key}", realpath($value));
+			}
+			elseif (file_exists($value)) {
+				$this->instance("path.{$key}", $value);
+			}
+			else {
+				$this->instance("path.{$key}", FALSE);
+			}
 		}
 	}
 
